2416ce4ce1ce4dfbfe3c4ad1a4b9be02687eb1d1
Antonio Chavez <antonio.2310cv@gmail.com>
2020-09-28 06:05:41 -0600

    Updates

---
+++ precache-manifest.7cc03819063d8d5142e40efb11a63aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe620b2054d7a041aca0405ec0572b8",
+    "url": "/marvel-app/index.html"
+  },
+  {
+    "revision": "7b93802a42af72b485fc",
+    "url": "/marvel-app/static/css/main.f9006772.chunk.css"
+  },
+  {
+    "revision": "18c39c1bfc4e38ba12e3",
+    "url": "/marvel-app/static/js/2.f1361ea8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/marvel-app/static/js/2.f1361ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b93802a42af72b485fc",
+    "url": "/marvel-app/static/js/main.51098e63.chunk.js"
+  },
+  {
+    "revision": "5c20dd92125b53193b65",
+    "url": "/marvel-app/static/js/runtime-main.cb1a6a69.js"
+  }
+]);
